e07fe47096e6069eb9248c17149ef4c012187b64
kaeobara <68631676+kaeobara@users.noreply.github.com>
2020-09-05 17:46:13 +0900

    golfモデルの作成

---
+++ db/migrate/20200905083301_create_golves.rb
@@ -0,0 +1,10 @@
+class CreateGolves < ActiveRecord::Migration[6.0]
+  def change
+    create_table :golves do |t|
+      t.string :title,           null: false
+      t.text :content,           null: false
+      t.references :user,        foreign_key: true
+      t.timestamps
+    end
+  end
+end
